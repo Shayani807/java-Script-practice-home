2592faa9a69a19de654778b5a8fdf6b5743ab7a5
shayani807 <shayanisulocha@gmail.com>
2026-08-13 18:10:28 +0530

    IF Else

--- script.js
+++ script.js
@@ -1,31 +1,23 @@
 // --------------------------------
-// ---------BMI Comparison---------
+// -------------If Else------------
 // --------------------------------
 
-/*
-Kasun and Chamal are trying to compare their BMI (Body Mass Index), which is calculated using the formula: 
-BMI = mass / height ^ 2
-(Mass is kg and Height in meter)
+var a = 10;
+var b = 20;
 
-1. Store Kasun's and Chamal's mass and height in variables
-2. Calculate both their BMIs
-3. Create a boolean variable containing information about whether Kasun has a higher BMI than Chamal.
-4. Print a string to in alert box containing the variable from step 3.
-*/
+// Condition
+if(a > b){
+    console.log("a is greater than b");
+}
+// Condition
+else if(a < b){
+    console.log("a is less than b");
+}
+else{
+    console.log("a is equal to b");
+}
 
-var mass, height, bmik, bmic, isKasun;
-
-// Kasun's BMI
-mass = prompt("Kasun's mass : ");
-height = prompt("Kasun's height : ");
-bmik = mass / (height * height);
-
-// Chamal's BMI
-mass = prompt("Chamal's mass : ");
-height = prompt("Chamal's height : ");
-bmic = mass / (height * height);
-
-// Boolean
-isKasun = (bmik > bmic);
-
-alert(isKasun);
+// Condition
+if(a == 10){
+    console.log("a is equal to 10");
+}
