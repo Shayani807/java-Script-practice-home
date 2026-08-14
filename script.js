d4b3d53246c1dd2d990edf6d6f75f7f8169d13fa
shayani807 <shayanisulocha@gmail.com>
2026-08-14 14:00:27 +0530

    Arrays

--- script.js
+++ script.js
@@ -1,25 +1,44 @@
-// -----------------------------------------
-// -----------Function Expressions----------
-// -----------------------------------------
+// ---------------------
+// --------Arrays-------
+// ---------------------
 
-// --Function declaration--
-// function profession(job, firstName){}
+/*
+    In JavaScript, array is a single variable that is used to store different elements. It is often used when we want to store list of elements and access them by a single variable.
+*/
 
-// --Function expression--
-var profession = function(job, firstName){
-    switch(job){
-        case 'doctor':
-            return firstName+' is treating patients.';
-        case 'teacher':
-            return firstName+' is teaching students.';
-        case 'engineer':
-            return firstName+' is working with buildings.';
-        default:
-            return firstName+' is doing something else.';
-    }
-}
+// Ways to define an array
+var students = ['Ruwan', 'Namal', 'Piyal', 'Kasuni', 'Danushka'];
+var marks = new Array(60,55,66,44,78);
 
-// Calling
-console.log(profession('doctor', 'Kasun'));
-console.log(profession('engineer', 'Sashini'));
-console.log(profession('professor', 'Kamal'));
+// Print array
+console.log(students);
+console.log(students.length);
+console.log(students[3]+' got '+marks[3]+' marks for Maths.');
+
+// Array mutation
+students[3] = 'Sanduni';
+students[5] = 'Thilini';
+students[students.length] = 'Chamara';
+console.log(students);
+
+// Different data types in an array
+var std1 = ['Kamal', 'Perera', 22, 'Civil', 'Panadura', true];
+console.log(std1);
+
+// Add elements
+std1.push('green');
+std1.unshift('Mr.');
+console.log(std1);
+
+// Remove elements
+std1.pop();
+std1.pop();
+std1.shift();
+console.log(std1);
+
+// Get index of an element
+console.log(std1.indexOf(22));
+
+var isAgriStudent = std1.indexOf('Agri') === -1 ? std1[0]+' is not a Agri student' : std1[0]+' is a Civil student';
+
+console.log(isAgriStudent);
