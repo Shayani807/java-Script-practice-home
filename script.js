6be2abd2d8fefe34b7e3474ce40d81c3c6f7feaa
shayani807 <shayanisulocha@gmail.com>
2026-08-14 13:52:35 +0530

    wining Team

--- script.js
+++ script.js
@@ -1,41 +1,41 @@
-// --------------------------------
-// --------Bitwise Operators-------
-// --------------------------------
+// ---------------------------
+// --------Winning Team-------
+// ---------------------------
 
 /*
-A bitwise operator treats their operands as a set of 32 bits (zeros and ones), rather than as decimal, hexadecimal, or octal numbers. For example, the decimal number nine has a binary representation of 1001. Bitwise operators perform their operations on such binary representations, but they return standard JavaScript numerical values.
+    Sri Lanka & England both played 3 match one day cricket series. In last 3 games Sri Lanka scored 250, 277, 300 runs, while England scored 185, 244, 360 runs.
+
+    1. Calculate the average score for each team
+    2. Decide which teams wins in average (highest average score), and display the winner with the average score
+    3. Then change scores to show different winners. Don't forget to take into account there might be a draw (same average score)
 */
 
-// 5 - 00000101
-// 1 - 00000001
+var score1, score2, score3, averageSri, averageEng;
 
-// And
-console.log(5 & 1);
+// Sri Lanka average
+score1 = parseInt(prompt("First match score of team Sri Lanka : "));
+score2 = parseInt(prompt("Second match score of team Sri Lanka : "));
+score3 = parseInt(prompt("Third match score of team Sri Lanka : "));
 
-// Or
-console.log(5 | 1)
+averageSri = (score1+score2+score3)/3;
 
-// Not
-// 5  - 0000000000000000000000000000 0101
-// ~5 - 1111111111111111111111111111 1010
-// (2 ^ 32)- 1 - 5
-console.log(~5);
+// England average
+score1 = parseInt(prompt("First match score of team England : "));
+score2 = parseInt(prompt("Second match score of team England : "));
+score3 = parseInt(prompt("Third match score of team England : "));
 
-// Xor
-console.log(5 ^ 1);
+averageEng = (score1+score2+score3)/3;
 
-// Zero fill left shift
-console.log(5 << 1);
-
-// Signed right shift
-console.log(5 >> 1);
-
-// Zero fill right shift
-console.log(5 >>> 1);
-
-// --Convert decimal to binary--
-console.log((10).toString(2));
-
-// --Convert binary to decimal--
-console.log((0b1100).toString(10));
-console.log(parseInt("1100", 2));
+// Find the winner
+if(averageSri > averageEng){
+    alert("Winner is team Sri Lanka | Average Score : "+averageSri);
+    console.log("Winner is team Sri Lanka | Average Score : "+averageSri);
+}
+else if(averageSri < averageEng){
+    alert("Winner is team England | Average Score : "+averageEng);
+    console.log("Winner is team England | Average Score : "+averageEng);
+}
+else{
+    alert("Match drawn, average scores are equal!");
+    console.log("Match drawn, average scores are equal!");
+}
