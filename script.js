1ce1199ed9956ecb2ce673a3a4610e56b8c35a1d
shayani807 <shayanisulocha@gmail.com>
2026-08-20 10:25:08 +0530

    Function Experession

--- script.js
+++ script.js
@@ -1,50 +1,25 @@
-// ----------------------------------------------
-// -----------Introduction of Functions----------
-// ----------------------------------------------
+// -----------------------------------------
+// -----------Function Expressions----------
+// -----------------------------------------
 
-/*
-    Funtion is a re-usable peace of code that does a specific thing.
-*/
+// --Function declaration--
+// function profession(job, firstName){}
 
-// --Define functions--
-
-// Without return
-function addNumbers(num1, num2){
-    var ans = num1 + num2;
-    console.log("Additon : "+ans);
-}
-
-// With return
-function subNumbers(num1, num2){
-    // Returns the value to where we called the function
-    // Exit the function
-    return num1 - num2;
-}
-
-// Calling functions
-addNumbers(20, 32.5);
-
-var sub = subNumbers(30, 12);
-console.log("Substraction :"+sub);
-
-// ----------------------------------
-
-// Define
-function calculateAge(birthYear){
-    return 2021 - birthYear;
+// --Function expression--
+var profession = function(job, firstName){
+    switch(job){
+        case 'doctor':
+            return firstName+' is treating patients.';
+        case 'teacher':
+            return firstName+' is teaching students.';
+        case 'engineer':
+            return firstName+' is working with buildings.';
+        default:
+            return firstName+' is doing something else.';
+    }
 }
 
 // Calling
-console.log("Kasun is "+calculateAge(1996)+" years old.");
-console.log("Priyan is "+calculateAge(2000)+" years old.");
-
-// Define
-function retiringYear(firstName, birthYear){
-    // Calling
-    var age = calculateAge(birthYear);
-    var retire = 60 - age;
-    console.log(firstName+" retires in "+retire+" years, in "+(birthYear+age+retire));
-}
-
-// Calling
-retiringYear("Namal", 1990);
+console.log(profession('doctor', 'Kasun'));
+console.log(profession('engineer', 'Sashini'));
+console.log(profession('professor', 'Kamal'));
